799760664578317bec49d7498488701abd2cdecc
rfsaldanha <rfsaldanha@gmail.com>
2021-09-01 20:33:00 -0300

    SIA test with all arguments

--- tests/testthat/test-sia.R
+++ tests/testthat/test-sia.R
@@ -1,5 +1,5 @@
 test_that("sia works", {
   sia <- fetch_datasus(year_start = 2016, month_start = 6, year_end = 2016, month_end = 7, uf = "AC", information_system = "SIA-PA")
-  sia <- process_sia(data = sia)
+  sia <- process_sia(data = sia, nome_proced = TRUE, nome_ocupacao = TRUE, nome_equipe = TRUE, information_system = "SIA-PA", municipality_data = TRUE)
   expect_true(class(sia) == "data.frame")
 })
