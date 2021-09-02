0c8108e62b96c42cf01e4c6ae8090617fa7901cf
rfsaldanha <rfsaldanha@gmail.com>
2021-09-02 17:43:52 -0300

    Remove random tests

--- tests/testthat/test-sim.R
+++ tests/testthat/test-sim.R
@@ -3,12 +3,3 @@ test_that("sim works at 2016, 6-7", {
   sim <- process_sim(data = sim, municipality_data = TRUE)
   expect_true(class(sim) == "data.frame")
 })
-
-test_that("sim works at random", {
-  uf <- sample(x = c("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SE","TO"), size = 1)
-  year <- round(runif(n = 1, min = 2010, max = lubridate::year(Sys.Date())-3))
-
-  sim <- fetch_datasus(year_start = year, year_end = year, uf = uf, information_system = "SIM-DO")
-  sim <- process_sim(data = sim, municipality_data = TRUE)
-  expect_true(class(sim) == "data.frame")
-})
--- tests/testthat/test-sinasc.R
+++ tests/testthat/test-sinasc.R
@@ -3,12 +3,3 @@ test_that("sinasc works at 2016, 6-7", {
   sinasc <- process_sinasc(data = sinasc, municipality_data = TRUE)
   expect_true(class(sinasc) == "data.frame")
 })
-
-test_that("sinasc works at random", {
-  uf <- sample(x = c("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SE","TO"), size = 1)
-  year <- round(runif(n = 1, min = 2010, max = lubridate::year(Sys.Date())-3))
-
-  sinasc <- fetch_datasus(year_start = year, year_end = year, uf = uf, information_system = "SINASC")
-  sinasc <- process_sinasc(data = sinasc, municipality_data = TRUE)
-  expect_true(class(sinasc) == "data.frame")
-})
