0bae097b2a1036de71d07e6da4703875ca976472
rfsaldanha <rfsaldanha@gmail.com>
2021-09-05 21:23:15 -0300

    sinan-dengue function, help and data

--- tests/testthat/test-sinan-dengue.R
+++ tests/testthat/test-sinan-dengue.R
@@ -1,7 +1,9 @@
 test_that("sinan-dengue-final at 2016", {
   sinan_dengue_final <- fetch_datasus(year_start = 2016, year_end = 2016, uf = "AC", information_system = "SINAN-DENGUE-FINAL")
+  sinan_dengue_final <- process_sinan_dengue(sinan_dengue_final, municipality_data = TRUE)
 })
 
 test_that("sinan-dengue-preliminar at 2020", {
   sinan_dengue_preliminar <- fetch_datasus(year_start = 2020, year_end = 2020, uf = "AC", information_system = "SINAN-DENGUE-PRELIMINAR")
+  sinan_dengue_preliminar <- process_sinan_dengue(sinan_dengue_preliminar, municipality_data = TRUE)
 })
