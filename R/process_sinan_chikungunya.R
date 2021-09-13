d88059a171ce38cc701ed95f9e2e4129e244a3e8
rfsaldanha <rfsaldanha@gmail.com>
2021-09-13 17:49:27 -0300

    Update documentation

--- R/process_sinan_chikungunya.R
+++ R/process_sinan_chikungunya.R
@@ -8,7 +8,8 @@
 #' @param municipality_data optional logical. \code{TRUE} by default, creates new variables in the dataset informing the full name and other details about the municipality of residence.
 #'
 #' @examples \dontrun{
-#' df <- fetch_datasus(year_start = 2016, year_end = 2016, uf = "RJ", information_system = "SINAN-CHIKUNGUNYA-FINAL")
+#' df <- fetch_datasus(year_start = 2016, year_end = 2016,
+#' uf = "RJ", information_system = "SINAN-CHIKUNGUNYA-FINAL")
 #' df_a <- process_sinan_chikungunya(df)
 #' df_b <- process_sinan_chikungunya(df, municipality_data = FALSE)
 #' }
