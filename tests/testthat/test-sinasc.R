21f442cfa84e2228afeb713b52b51abb9439b361
Raphael Saldanha <rfsaldanha@gmail.com>
2022-10-13 12:16:13 -0300

    Test sinasc

--- tests/testthat/test-sinasc.R
+++ tests/testthat/test-sinasc.R
@@ -1,5 +1,8 @@
-test_that("sinasc works at 2016, 6-7", {
+test_that("sinasc works", {
   sinasc <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SINASC")
   sinasc <- process_sinasc(data = sinasc, municipality_data = TRUE)
   expect_true(class(sinasc) == "data.frame")
 })
+
+
+sinasc <- fetch_datasus(year_start = 1990, year_end = 2025, uf = "AC", information_system = "SINASC")
