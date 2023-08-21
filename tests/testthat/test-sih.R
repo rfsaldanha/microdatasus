2b7ea893854247d8560d54c8645befb66af723ff
Raphael Saldanha <rfsaldanha@gmail.com>
2023-08-21 14:47:08 +0200

    Fetch SIH works with two UFs.

--- tests/testthat/test-sih.R
+++ tests/testthat/test-sih.R
@@ -19,4 +19,10 @@ test_that("sih er works at 2016, 6-7", {
   expect_true(class(sih) == "data.frame")
 })
 
+test_that("sih rd works with two UFs", {
+  sih <- fetch_datasus(year_start = 2016, month_start = 6, year_end = 2016, month_end = 7, uf = c("AC", "AP"), information_system = "SIH-RD")
+  sih <- process_sih(data = sih, information_system = "SIH-RD", municipality_data = TRUE)
+  expect_true(class(sih) == "data.frame")
+})
+
 
