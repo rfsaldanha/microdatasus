06af31330b1d333409f768fe416077374c11d64c
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-05 20:51:18 +0200

    Separate raw and processed files

--- tests/testthat/test-sim.R
+++ tests/testthat/test-sim.R
@@ -1,29 +1,29 @@
 test_that("sim do works", {
-  sim <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DO")
-  sim <- process_sim(data = sim, municipality_data = TRUE)
+  sim_raw <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DO")
+  sim <- process_sim(data = sim_raw, municipality_data = TRUE)
   expect_true(class(sim) == "data.frame")
 })
 
 test_that("sim dofet works", {
-  sim <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOFET")
-  sim <- process_sim(data = sim, municipality_data = TRUE)
+  sim_raw <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOFET")
+  sim <- process_sim(data = sim_raw, municipality_data = TRUE)
   expect_true(class(sim) == "data.frame")
 })
 
 test_that("sim doext works", {
-  sim <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOEXT")
-  sim <- process_sim(data = sim, municipality_data = TRUE)
+  sim_raw <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOEXT")
+  sim <- process_sim(data = sim_raw, municipality_data = TRUE)
   expect_true(class(sim) == "data.frame")
 })
 
 test_that("sim doinf works", {
-  sim <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOINF")
-  sim <- process_sim(data = sim, municipality_data = TRUE)
+  sim_raw <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOINF")
+  sim <- process_sim(data = sim_raw, municipality_data = TRUE)
   expect_true(class(sim) == "data.frame")
 })
 
 test_that("sim domat works", {
-  sim <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOMAT")
-  sim <- process_sim(data = sim, municipality_data = TRUE)
+  sim_raw <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SIM-DOMAT")
+  sim <- process_sim(data = sim_raw, municipality_data = TRUE)
   expect_true(class(sim) == "data.frame")
 })
