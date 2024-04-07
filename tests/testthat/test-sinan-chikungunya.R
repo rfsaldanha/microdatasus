1142fb4ba19a735a5e5a633967478d315da027a9
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-07 21:15:09 +0200

    Update test

--- tests/testthat/test-sinan-chikungunya.R
+++ tests/testthat/test-sinan-chikungunya.R
@@ -1,5 +1,5 @@
-test_that("sinan-chikungunya at 2016", {
-  sinan_chikungunya_raw <- fetch_datasus(year_start = 2021, year_end = 2021, information_system = "SINAN-CHIKUNGUNYA")
+test_that("sinan-chikungunya at 2022", {
+  sinan_chikungunya_raw <- fetch_datasus(year_start = 2022, year_end = 2022, information_system = "SINAN-CHIKUNGUNYA")
   sinan_chikungunya <- process_sinan_chikungunya(sinan_chikungunya_raw, municipality_data = TRUE)
 })
 
