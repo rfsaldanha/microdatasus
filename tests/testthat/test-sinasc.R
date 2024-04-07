6f721a5730593c28e904b818d4ca0e5547b15534
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-07 20:14:36 +0200

    Split raw and processed Expect a tibble.

--- tests/testthat/test-sinasc.R
+++ tests/testthat/test-sinasc.R
@@ -1,6 +1,6 @@
 test_that("sinasc works", {
-  sinasc <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SINASC")
-  sinasc <- process_sinasc(data = sinasc, municipality_data = TRUE)
-  expect_true(class(sinasc) == "data.frame")
+  sinasc_raw <- fetch_datasus(year_start = 2016, year_end = 2017, uf = "AC", information_system = "SINASC")
+  sinasc <- process_sinasc(data = sinasc_raw, municipality_data = TRUE)
+  expect_true(class(sinasc)[1] == "tbl_df")
 })
 
