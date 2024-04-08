20e6ec5f64bfe41929df3eccfb9b8fd405615305
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-08 11:53:35 +0200

    tests update

--- tests/testthat/test-sinan-dengue.R
+++ tests/testthat/test-sinan-dengue.R
@@ -1,4 +1,4 @@
 test_that("sinan-dengue at 2016", {
-  sinan_dengue <- fetch_datasus(year_start = 2010, year_end = 2010, information_system = "SINAN-DENGUE")
-  sinan_dengue <- process_sinan_dengue(sinan_dengue, municipality_data = TRUE)
+  sinan_dengue_raw <- fetch_datasus(year_start = 2010, year_end = 2010, information_system = "SINAN-DENGUE")
+  sinan_dengue <- process_sinan_dengue(sinan_dengue_raw, municipality_data = TRUE)
 })
