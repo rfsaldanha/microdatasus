85edab1aa8aaa884b4b6c4c40e70f537b013873d
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-08 18:50:59 +0200

    new data files

--- data-raw/sigtab.R
+++ data-raw/sigtab.R
@@ -1,8 +1,8 @@
 ## sigtab was created based on TB_SIGTAP.dbf, from ftp://ftp.datasus.gov.br/dissemin/publicos/SIASUS/200801_/Auxiliar/TAB_SIA.zip
 
 sigtab <- foreign::read.dbf(file = "data-raw/TB_SIGTAW.dbf", as.is = TRUE)
-#Encoding(sigtab$DS_REGRA) <- "latin1"
-#sigtab$DS_REGRA <- stringi::stri_escape_unicode(str = sigtab$DS_REGRA)
+Encoding(sigtab$IP_DSCR) <- "latin1"
+sigtab$IP_DSCR <- stringi::stri_escape_unicode(str = sigtab$IP_DSCR)
 colnames(sigtab) <- c("COD", "nome_proced")
 
 usethis::use_data(sigtab, overwrite = TRUE, compress = "xz")
