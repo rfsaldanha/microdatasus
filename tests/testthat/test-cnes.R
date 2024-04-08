5abae7733bae7009f9066979880f4f6d05fe4fb6
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-08 16:36:02 +0200

    update test

--- tests/testthat/test-cnes.R
+++ tests/testthat/test-cnes.R
@@ -1,11 +1,11 @@
 test_that("cnes-st works at 2016, 6-7", {
   cnes_st_raw <- fetch_datasus(year_start = 2016, month_start = 6, year_end = 2016, month_end = 6, uf = "AC", information_system = "CNES-ST")
   cnes_st <- process_cnes(data = cnes_st_raw, information_system = "CNES-ST", nomes = TRUE, municipality_data = TRUE)
-  expect_true(class(cnes_st_processed) == "data.frame")
+  expect_true(class(cnes_st)[1] == "tbl_df")
 })
 
 test_that("cnes-sf works at 2016, 6-7", {
-  cnes_pf <- fetch_datasus(year_start = 2016, month_start = 6, year_end = 2016, month_end = 6, uf = "AC", information_system = "CNES-PF")
-  cnes_pf_processed <- process_cnes(data = cnes_pf, information_system = "CNES-PF", nomes = TRUE, municipality_data = TRUE)
-  expect_true(class(cnes_pf_processed) == "data.frame")
+  cnes_pf_raw <- fetch_datasus(year_start = 2016, month_start = 6, year_end = 2016, month_end = 6, uf = "AC", information_system = "CNES-PF")
+  cnes_pf <- process_cnes(data = cnes_pf_raw, information_system = "CNES-PF", nomes = TRUE, municipality_data = TRUE)
+  expect_true(class(cnes_pf)[1] == "tbl_df")
 })
