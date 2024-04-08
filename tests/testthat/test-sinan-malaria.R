167b95fdaed35075a916791968f92ce4aec031f5
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-08 12:06:18 +0200

    tests update

--- tests/testthat/test-sinan-malaria.R
+++ tests/testthat/test-sinan-malaria.R
@@ -1,5 +1,5 @@
 test_that("sinan-malaria at 2016", {
-  sinan_malaria <- fetch_datasus(year_start = 2016, year_end = 2016, information_system = "SINAN-MALARIA")
-  sinan_malaria <- process_sinan_malaria(sinan_malaria, municipality_data = TRUE)
+  sinan_malaria_raw <- fetch_datasus(year_start = 2016, year_end = 2016, information_system = "SINAN-MALARIA")
+  sinan_malaria <- process_sinan_malaria(sinan_malaria_raw, municipality_data = TRUE)
 })
 
